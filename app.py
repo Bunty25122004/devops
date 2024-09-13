734e1b87c2584677162bd61e7127dde2c9245b95
hello <hello@gmail.com>
2024-09-13 10:13:00 +0530

    rohith1

--- app.py
+++ app.py
@@ -15,3 +15,20 @@ def success();
     if__name__=='main__':
         app.run(host='0.0.0.0')    
 
+from flask import flask,render_template,request
+app=flask(_name_)
+@app.route('/')
+def home();
+    return render_template('register.html')
+@app.route('/success',methods=['GET','POST'])
+def success();
+    if request.method=="POST":
+        nmae=request.form['name']
+        email=request.form['name']
+        password=request.form['password']
+        return render_template('register.html')
+    else:
+        return render_template('register.html')
+    if__name__=='main__':
+        app.run(host='0.0.0.0')    
+
